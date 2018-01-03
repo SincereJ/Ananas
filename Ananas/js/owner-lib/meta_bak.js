30845d07df40618822aea336e00f4f74322a88f6
Administrator <Administrator@DESKTOP-H3M9TT6>
2018-01-03 18:10:12 +0800

    a

---
+++ Ananas/js/owner-lib/meta_bak.js
@@ -0,0 +1,116 @@
+(function(){
+	var ananasApp = angular.module("ananasApp",["ng","treeControl"]);
+	
+	ananasApp.controller("ananasCtrl", function($scope,$http,$location) {
+		
+		$scope.resultData = {};
+		$scope.display = false;
+		$scope.newMetaContext = {};
+		
+	    $scope.save = function (){
+	    	init($scope);
+	    }
+	    
+	    $scope.goMeta = function(){
+	    	//$location.path("../../metaIndx.html");
+	    }
+	
+	    function init (_$scope){
+			$http({
+			    method: 'POST',
+			    url: 'http://localhost:8080/Ananas/meta/getDivMeta'
+			}).then(function successCallback(response) {
+		        console.log(response);
+		        
+		        if(response != null && response.status == 200){
+		    		var resData = response.data;
+		    		if(resData != undefined){
+		    			_$scope.resultData.name = resData[0].name;
+		    			_$scope.resultData.context = resData[0].context;
+		    		}
+		    	}
+		        
+		    }, function errorCallback(response) {
+		    	//console.log(response);
+			})
+		}
+	    
+	    $scope.dipaly = function (){
+	    	$scope.display = !$scope.display;
+	    	var metaContext = angular.fromJson($scope.resultData.context);
+	    	console.log(metaContext);
+	    	//var props = metaContext.prop;
+	    	//var temp = metaContext.temp;
+	    	
+	    	$scope.metaContext = metaContext;
+	    }
+	    
+	    $scope.subt = function(){
+	    	console.log($scope.newMetaContext);
+	    	
+	    	var　subMeta = $scope.newMetaContext;
+	    	
+	    	var metaContext = $scope.metaContext;
+	    	//$scope.resultData.name = 
+	    	rebuild(subMeta);
+	    	
+	    }
+	    
+	    function rebuild(subMeta){
+	    	var subResultData = {};
+	    	subResultData.name = subMeta.newname;
+	    	subResultData.context = [];
+	    	var subItemTemp = {"temp":$scope.metaContext.temp};
+	    	var subItemProp = {};
+	    	
+	    	/*angular.forEach(subMeta.prop,function(pp){
+	    		subItemProp[pp] = subMeta.prop[pp];
+	    	});*/
+	    	
+	    	for (x in subMeta.prop){
+	    		subItemProp[x] = subMeta.prop[x];
+	    	}
+	    	subItemTemp.prop = subItemProp;
+	    	subResultData.context.push(subItemTemp);
+	    	
+	    	console.log(subResultData);
+	    	
+	    }
+	    
+	    //tree
+	    $scope.showSelected = function(sel) {
+	        $scope.selectedNode = sel;
+	    };
+	    
+	    $scope.treeOptions = {
+    	    nodeChildren: "children",
+    	    dirSelectable: true,
+    	    injectClasses: {
+    	        ul: "a1",
+    	        li: "a2",
+    	        liSelected: "a7",
+    	        iExpanded: "a3",
+    	        iCollapsed: "a4",
+    	        iLeaf: "a5",
+    	        label: "a6",
+    	        labelSelected: "a8"
+    	    }
+    	};
+    	$scope.dataForTheTree =
+    	[
+    	    { "name" : "Joe", "age" : "21", "children" : [
+    	        { "name" : "Smith", "age" : "42", "children" : [] },
+    	        { "name" : "Gary", "age" : "21", "children" : [
+    	            { "name" : "Jenifer", "age" : "23", "children" : [
+    	                { "name" : "Dani", "age" : "32", "children" : [] },
+    	                { "name" : "Max", "age" : "34", "children" : [] }
+    	            ]}
+    	        ]}
+    	    ]},
+    	    { "name" : "Albert", "age" : "33", "children" : [] },
+    	    { "name" : "Ron", "age" : "29", "children" : [] }
+    	];
+
+	});
+	
+})();
