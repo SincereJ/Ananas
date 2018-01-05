5b99ea245e8de1c6bd56a7a4aee81707c462ecb0
Administrator <Administrator@DESKTOP-H3M9TT6>
2018-01-05 17:29:42 +0800

    a

---
+++ Ananas/src/com/ananas/mn/core/bean/MetaModelResultType.java
@@ -0,0 +1,29 @@
+package com.ananas.mn.core.bean;
+
+public class MetaModelResultType {
+
+	private String name;
+	private String temp;
+	private Object prop;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTemp() {
+		return temp;
+	}
+	public void setTemp(String temp) {
+		this.temp = temp;
+	}
+	public Object getProp() {
+		return prop;
+	}
+	public void setProp(Object prop) {
+		this.prop = prop;
+	}
+	
+	
+	
+}
